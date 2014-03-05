7b6aaebef707fbd2fa51d6625b1e7b5f6abe9ad2
Stefvg <stef.vangils@student.kuleuven.be>
2014-03-05 14:47:26 +0100

    Advance geïmplementeerd

--- SWOP/src/assembly/AssemblyLine.java
+++ SWOP/src/assembly/AssemblyLine.java
@@ -16,31 +16,31 @@ public class AssemblyLine {
 		workbenches = new ArrayList<WorkBench>();
 		currentJobs = new ArrayList<Job>();
 	}
-	
+
 	public List<WorkBench> getWorkbenches() {
 		return workbenches;
 	}
-	
+
 	public void setWorkbenches(List<WorkBench> workbenches) {
 		if(workbenches==null)
 			throw new IllegalArgumentException();
 		this.workbenches = workbenches;
 	}
-	
+
 	public List<Job> getCurrentJobs() {
 		return currentJobs;
 	}
-	
+
 	public void setCurrentJobs(List<Job> currentJobs) {
 		if(currentJobs==null)
 			throw new IllegalArgumentException();
 		this.currentJobs = currentJobs;
 	}
-	
+
 	public int getOvertime() {
 		return overtime;
 	}
-	
+
 	public void setOvertime(int overtime) {
 		if(overtime<0)
 			throw new IllegalArgumentException();
@@ -56,13 +56,41 @@ public class AssemblyLine {
 			throw new IllegalArgumentException();
 		else getCurrentJobs().add(job);
 	}
-	
+
 	public void addWorkBench(WorkBench bench){
 		if(bench==null)
 			throw new IllegalArgumentException();
 		else getWorkbenches().add(bench);
+	}	
+	
+	public void advance(){
+		for(WorkBench bench: getWorkbenches())
+			if(!bench.isCompleted())
+				return;
+		
+		Job lastJob = null;
+		for(int i=0; i<getWorkbenches().size(); i++){
+			WorkBench bench = getWorkbenches().get(i);
+			if(i==0){
+				lastJob = bench.getCurrentJob();
+				if(bench.getCurrentJob()==null){
+					bench.setCurrentJob(getCurrentJobs().get(0));
+				}else{
+					int index = getCurrentJobs().indexOf(bench.getCurrentJob());
+					if((index+1)<getCurrentJobs().size()){
+						bench.setCurrentJob(getCurrentJobs().get(index+1));
+					}
+				}
+			}else{
+				Job prev = bench.getCurrentJob();
+				if(lastJob!=null)
+					bench.setCurrentJob(lastJob);
+				lastJob = prev;
+			}
+			bench.chooseTasksOutOfJob();
+		}
+		if(lastJob !=null && lastJob.isCompleted())
+			getCurrentJobs().remove(lastJob);
 	}
-	
-	
-	
+
 }
--- SWOP/src/assembly/WorkBench.java
+++ SWOP/src/assembly/WorkBench.java
@@ -7,7 +7,6 @@ public class WorkBench {
 
 	private List<String> responsibilities;
 	private Job currentJob;
-	private Task currentTask;
 	private List<Task> currentTasks;
 	public WorkBench(List<String> responsibilities){
 		this.setResponsibilities(responsibilities);
@@ -49,13 +48,26 @@ public class WorkBench {
 		this.currentTasks = currentTasks;
 	}
 	
+	/**
+	 * Selects the tasks that are valid for this Workbench. 
+	 * The task description is checked against the responsibilities.
+	 */
 	public void chooseTasksOutOfJob(){
+		if(getCurrentJob()==null)
+			return;
 		List<Task> tasks = new ArrayList<>();
 		for(Task task: getCurrentJob().getTasks())
 			if(getResponsibilities().contains(task.getTaskDescription()) && !task.isCompleted()){
 				tasks.add(task);
 			}
 		setCurrentTasks(tasks);
+	}
+	
+	public boolean isCompleted(){
+		for(Task task: getCurrentTasks())
+			if(!task.isCompleted())
+				return false;
+		return true;
 	}
 	
 }
