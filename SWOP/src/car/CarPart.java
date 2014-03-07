edb80b8cf382f0f19dc94f13e9f3a1de873b97c9
RutgerMoons <rutger.moons@student.kuleuven.be>
2014-03-07 15:48:16 +0100

    CarPartCatalogue

--- SWOP/src/car/CarPart.java
+++ SWOP/src/car/CarPart.java
@@ -5,10 +5,21 @@ public abstract class CarPart {
 	
 	private String description;
 	
+	/**
+	 * 
+	 * @param type One of the possible types for this CarPart
+	 * @effect the description of this object equals type,
+	 * 			| unless type == null
+	 * 			| or type is not one of the possible types for this CarPart
+	 */
 	public void setDescription(String type){
 		this.description = type; 
 	}
 	
+	/**
+	 * 
+	 * @return The description of this object
+	 */
 	public String getDescription(){
 		return this.description;
 	}
