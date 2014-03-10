3134222a34c03f5ae293a153901c44b13032c666
KarenRaets <karentje9@hotmail.com>
2014-03-10 11:53:34 +0100

    Testen vervolledigd tot zover ik kan
    De rest zijn speciale cases die er minder toe doen.
    99,1% code coverage in package car

--- SWOP/src/car/CarPartCatalogue.java
+++ SWOP/src/car/CarPartCatalogue.java
@@ -1,22 +1,26 @@
 package car;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
+import java.util.Map.Entry;
 
 /**
  * this class will be used to validate CarParts
  * this class will be initialized upon creation
  */
 public class CarPartCatalogue {
-	
+
 	private HashMap<Class<?>, List<CarPart>> carPartCatalogue;
-	
+
 	public CarPartCatalogue() {
 		this.carPartCatalogue = new HashMap<>();
 		CarPartCatalogueFiller filler = new CarPartCatalogueFiller(this);
 		filler.initializeCarParts();
 	}
-	
+
 	/**
 	 * checks whether a given CarPart is valid (three cases):
 	 * 		CarPart is null -> false
@@ -27,7 +31,7 @@ public class CarPartCatalogue {
 	 */
 	public boolean isValidCarPart(CarPart carPart) {
 		return carPart != null && //carPartCatalogue.get(carPart.getClass()).contains(carPart.getDescription());
-	//} 
+				//} 
 				carPartCatalogue.get(carPart.getClass()).contains(carPart);
 	}
 
@@ -36,7 +40,6 @@ public class CarPartCatalogue {
 	 * 		the class is valid
 	 * 		the list is not null
 	 */
-
 	public void addListForCarPart(Class<?> key, List<CarPart> value) throws IllegalArgumentException {
 		if (key == null) {
 			throw new IllegalArgumentException(); 
@@ -45,9 +48,30 @@ public class CarPartCatalogue {
 			carPartCatalogue.put(key, value);
 		}
 	}
-	
+
+	/**
+	 * Returns a deep copy clone of the carPartCatalogue.
+	 */
 	public HashMap<Class<?>, List<CarPart>> getMap() {
-		return carPartCatalogue;
+		return clone(carPartCatalogue);
+	}
+
+	/**
+	 * Method for making a deep copy of a certain hashmap of type <Class<?>, List<CarPart>>.
+	 */
+	private HashMap<Class<?>, List<CarPart>> clone(HashMap<Class<?>, List<CarPart>> carPartCatalogue2) {
+		HashMap<Class<?>, List<CarPart>> newMap = new HashMap<Class<?>, List<CarPart>>();
+		
+		Set<Entry<Class<?>, List<CarPart>>> set1 = carPartCatalogue2.entrySet();
+		
+		for (Entry<Class<?>, List<CarPart>> entry : set1){
+			List<CarPart> test = new ArrayList<CarPart>();
+			for(CarPart part :entry.getValue()){
+				test.add(part);
+			}
+			newMap.put(entry.getKey(),test);
+		}
+		return newMap;
 	} 
-	
+
 }
--- SWOP/test/carTest/CarPartCatalogueTest.java
+++ SWOP/test/carTest/CarPartCatalogueTest.java
@@ -2,10 +2,15 @@ package carTest;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.junit.Test;
 
 import car.Airco;
 import car.Body;
+import car.CarPart;
 import car.CarPartCatalogue;
 
 
@@ -29,5 +34,30 @@ public class CarPartCatalogueTest {
 		Body body = null;
 		assertFalse(catalogue.isValidCarPart(body));
 	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void test3(){
+		CarPartCatalogue cat = new CarPartCatalogue();
+		ArrayList<CarPart> array = new ArrayList<CarPart>();
+		array.add(new Airco("manual"));
+		cat.addListForCarPart(null, array);
+	}
+	
+	@Test
+	public void test4(){
+		CarPartCatalogue cat = new CarPartCatalogue();
+		ArrayList<CarPart> array = new ArrayList<CarPart>();
+		array.add(new Airco("manual"));
+		cat.addListForCarPart(Airco.class, array);
+		HashMap<Class<?>, List<CarPart>> map = cat.getMap();
+		//System.out.println(map.toString());
+		assertEquals(7,map.entrySet().size());
+		ArrayList<CarPart> arr = new ArrayList<CarPart>();
+		arr.add(new Body("break"));
+		cat.addListForCarPart(CarPart.class, arr);
+		assertEquals(7,map.entrySet().size());
+	}
+	
+	
 
 }
