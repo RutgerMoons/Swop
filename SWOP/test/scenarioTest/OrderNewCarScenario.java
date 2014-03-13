83f47a6e80a98814585159748ab4b70926453e51
KarenRaets <karentje9@hotmail.com>
2014-03-13 15:38:00 +0100

    Begin scenarioTests

---
+++ SWOP/test/scenarioTest/OrderNewCarScenario.java
@@ -0,0 +1,14 @@
+package scenarioTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderNewCarScenario {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
