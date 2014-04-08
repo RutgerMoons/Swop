8262e3029b1db1e838fbd4107c08281103baf7fd
RutgerMoons <rutger.moons@student.kuleuven.be>
2014-04-08 22:05:45 +0200

    Scheduler voor een groot deel

--- SWOP/src/domain/scheduler/Scheduler.java
+++ SWOP/src/domain/scheduler/Scheduler.java
@@ -5,15 +5,26 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 import domain.car.CarPart;
+import domain.clock.UnmodifiableClock;
+import domain.exception.NoSuitableJobFoundException;
 import domain.job.Job;
+import domain.observer.LogsClock;
 
-public class Scheduler {
+public class Scheduler implements LogsClock {
 
 	private SchedulingAlgorithm schedulingAlgorithm;
 	private final int amountOfWorkBenches;
+	private ArrayList<Shift> shifts;
+	private UnmodifiableClock clock;
 	
 	public Scheduler(int amountOfWorkBenches) {
 		this.amountOfWorkBenches = amountOfWorkBenches;
+		switchToFifo();
+		shifts = new ArrayList<>();
+		Shift shift1 = new Shift(360, 840, 0); 	//shift van 06:00 tot 14:00
+		Shift shift2 = new Shift(840, 1320, 0);	//shift van 14:00 tot 22:00
+		shifts.add(shift1);
+		shifts.add(shift2);
 	}
 	
 	public void addCustomJob(Job customJob) {
@@ -46,6 +57,23 @@ public class Scheduler {
 			this.schedulingAlgorithm = new SchedulingAlgorithmBatch(carParts, amountOfWorkBenches);
 			this.schedulingAlgorithm.transform(customJobs, standardJobs);
 		}
+	}
+	
+	public Job retrieveNextJob(int currentTotalProductionTime) throws NoSuitableJobFoundException {
+		// einduur laatste shift - beginuur eerste shift
+		int minutesTillEndOfDay = shifts.get(shifts.size() - 1).getEndOfShift() - shifts.get(0).getStartOfShift();
+		return this.schedulingAlgorithm.retrieveNext(currentTotalProductionTime, minutesTillEndOfDay, clock);
+	}
+
+	@Override
+	public void advanceTime(UnmodifiableClock currentTime) {
+		this.clock = currentTime;
+	}
+
+	@Override
+	public void startNewDay() {
+		// TODO: moet hier iets staan?
+		// eigenlijk moeten we hier een nieuwe klok maken/krijgen die geïntialiseerd wordt op het startuur van shift 1
 	}
 	
 }
