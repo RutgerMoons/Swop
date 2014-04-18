139c80fe1b2e267e41617a508ddb4fe2b6458bfa
KarenRaets <karentje93@hotmail.com>
2014-04-18 21:48:46 +0200

    Delay 100% CC

--- SWOP/test/domain/orderTest/DelayTest.java
+++ SWOP/test/domain/orderTest/DelayTest.java
@@ -1,6 +1,8 @@
 package domain.orderTest;
 
 import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 import domain.clock.UnmodifiableClock;
@@ -17,6 +19,26 @@ public class DelayTest {
 		completed = new UnmodifiableClock(2,850);
 		delay = new Delay(estimated,completed);
 		assertEquals(450, delay.getDelay());
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void constructorTestFail1() {
+		delay = new Delay(null, completed);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void constructorTestFail2() {
+		delay = new Delay(null, null);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void constructorTestFail3() {
+		delay = new Delay(estimated, null);
+	}
+	
+	@Test
+	public void toStringTest(){
+		assertEquals("2: 450", delay.toString());
 	}
 
 }
