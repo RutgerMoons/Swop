c2537e387a625e392665d9adfad2e0ffa1135c8a
KarenRaets <karentje93@hotmail.com>
2014-04-23 15:59:34 +0200

    gthyuklpù

--- SWOP/src/ui/AssemAssist.java
+++ SWOP/src/ui/AssemAssist.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+import ui.ClientCommunication;
 import controller.FlowControllerFactory;
 import controller.UseCaseFlowController;
 import controller.UserFlowController;
