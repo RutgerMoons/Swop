839c99c932c0c2132b17cc42e12441fad9046c91
KarenRaets <karentje93@hotmail.com>
2014-05-07 14:39:12 +0200

    The beginning of the adventure of a fellow called Company

---
+++ SWOP/src/domain/company/Company.java
@@ -0,0 +1,18 @@
+package domain.company;
+
+import domain.order.OrderBook;
+import domain.restriction.PartPicker;
+import domain.users.UserBook;
+
+/**
+ * 
+ * 
+ *
+ */
+public class Company {
+	
+	private UserBook userbook;
+	private OrderBook orderbook;
+	private PartPicker partpicker;
+	
+}
