6a02d7f6e82069819309917a310374bb2d4fa796
KarenRaets <karen.raets@student.kuleuven.be>
2014-05-16 12:22:31 +0200

    Shift doc gefixt

--- SWOP/src/domain/scheduling/Shift.java
+++ SWOP/src/domain/scheduling/Shift.java
@@ -1,6 +1,6 @@
 package domain.scheduling;
 /**
- * Object representing a shift. Shift has a start hour, an end hour and an amount of overtime.
+ * A class representing a shift. Shift has a start hour, an end hour and an amount of overtime.
  */
 public class Shift {
 	
@@ -11,14 +11,23 @@ public class Shift {
 	/**
 	 * Constructs a Shift object.
 	 * 
-	 * @param start
-	 * 		The starting hour of a shift in minutes
-	 * @param end
-	 * 		The end hour of a shift in minutes
-	 * @param overtime
-	 * 		The amount of overtime in a shift in minutes
+	 * @param 	start
+	 * 			The starting hour of a shift in minutes
+	 * 
+	 * @param 	end
+	 * 			The end hour of a shift in minutes
+	 * 
+	 * @param 	overtime
+	 * 			The amount of overtime in a shift in minutes
+	 * 
+	 * @throws	IllegalArgumentException
+	 * 			Thrown when the value of end is smaller then start or 
+	 * 			when start or end have a negative value
 	 */
 	public Shift(int start, int end, int overtime) {
+		if (end < start || start < 0 || end < 0){
+			throw new IllegalArgumentException();
+		}
 		this.start = start;
 		this.end = end;
 		this.overtime = overtime;
@@ -32,10 +41,13 @@ public class Shift {
 	}
 	
 	/**
-	 * @param newOvertime
-	 * 		Value for the overtime in the next day of this shift.
-	 * @throws IllegalArgumentException
-	 * 		Thrown when the parameter has a negative value
+	 * Method for setting the overtime for the next day.
+	 * 
+	 * @param 	newOvertime
+	 * 			Value for the overtime in the next day of this shift
+	 * 
+	 * @throws 	IllegalArgumentException
+	 * 			Thrown when the parameter has a negative value
 	 */
 	public void setNewOvertime(int newOvertime) {
 		if(newOvertime < 0){
@@ -44,6 +56,9 @@ public class Shift {
 		this.overtime = newOvertime;
 	}
 	
+	/**
+	 * Returns the start of the shift in minutes.
+	 */
 	public int getStartOfShift() {
 		return this.start;
 	}
