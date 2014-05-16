9664482618ff3a59c3564e117d2fb7a9cb9e3320
KarenRaets <karen.raets@student.kuleuven.be>
2014-05-16 10:37:27 +0200

    package exception gedocumenteerd

--- SWOP/src/domain/exception/NoSuitableJobFoundException.java
+++ SWOP/src/domain/exception/NoSuitableJobFoundException.java
@@ -1,5 +1,9 @@
 package domain.exception;
 
+/**
+ * Class representing an Exception thrown when an SchedulingAlgorithm of an AssemblyLine didn't find
+ * a suitable Job.
+ */
 public class NoSuitableJobFoundException extends IllegalStateException {
 
 	private static final long serialVersionUID = -4069064046713112271L;
--- SWOP/src/domain/exception/UnmodifiableException.java
+++ SWOP/src/domain/exception/UnmodifiableException.java
@@ -1,5 +1,9 @@
 package domain.exception;
 
+/**
+ * Class representing an Exception thrown when a function tries to alter a
+ * unmodifiable object.
+ */
 public class UnmodifiableException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
