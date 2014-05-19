6e9e1cafcce92f1c4da80cb3fee28f1af114e42d
RutgerMoons <rutger.moons@student.kuleuven.be>
2014-05-19 16:57:52 +0200

    àlo!ikyuj§gtrf

---
+++ SWOP/src/domain/exception/NoMoreJobsToScheduleException.java
@@ -0,0 +1,10 @@
+package domain.exception;
+
+/**
+ * A class representing an Exception thrown when a scheduling object doesn't have anything to schedule.
+ */
+public class NoMoreJobsToScheduleException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+}
